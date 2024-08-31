33acfafe0da7a7da9f0cd83db2d95e5488d714a8
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2024-08-31 19:10:35 +0100

    exercise: printing

---
+++ src/01-Printing/exercise-1a.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//    - Print "Hello World" to the console.
+	fmt.Println("Hello World")
+
+	//    `"Hi! My name is <Your Name>. I have lived in <Hometown> for <Number> years.
+	// 		They say the weather is amazing, which is <true or false>."`
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Ibrahim", "Ile-Ife", 22, true)
+}
